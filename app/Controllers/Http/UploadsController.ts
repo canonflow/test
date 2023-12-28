8613ab3c01629b1e6756699ea9e60179c1ca05d7
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2023-12-28 23:35:56 +0700

    [From windows 8] testing 2

--- app/Controllers/Http/UploadsController.ts
+++ app/Controllers/Http/UploadsController.ts
@@ -1,23 +1,38 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Application from '@ioc:Adonis/Core/Application'
-import Drive from "@ioc:Adonis/Core/Drive";
 
 export default class UploadsController {
   public async upload({ request, response }: HttpContextContract) {
-    const image = request.file('image')
+    // Buat di simpan di luar build
+    const imageResources = request.file('image')
+    // Buat di simpan di build
+    const imagePublic = request.file('image')
 
-    if (image !== null) {
-      await image.moveToDisk('./', { name: `test.${image.extname}` })
-      const file = await Drive.get(`./test.${image.extname}`).toString()
-      await Drive.put(Application.appRoot + '/public/assets/files', file)
+    if (imageResources !== null && imagePublic) {
+      // Kalo di server
+      if (Application.inProduction) {
+        // Simpan server
+        await imageResources.moveToDisk('./', { name: `test1.${imageResources.extname}` })
+        // Simpan di build
+        await imagePublic.move(Application.appRoot + '/public/assets/files', {
+          name: `test1.${imagePublic.extname}`,
+        })
+
+        const resourcePath = imageResources.filePath
+        const publicPath = imagePublic.filePath
+        return response.send({ resourcePath, publicPath })
+      }
+      // await image.moveToDisk('./', { name: `test.${image.extname}` })
+      // const file = await Drive.get(`./test.${image.extname}`).toString()
+      // await Drive.put(Application.appRoot + '/public/assets/files', file)
       // await image.move(Application.appRoot + '/public/assets/files', {
       //   name: `test.${image.extname}`,
       // })
-      const fileName = image.fileName
+      // const fileName = image.fileName
       // const fileTmp = image.tmpPath
-      const filePath = image.filePath
+      // const filePath = image.filePath
 
-      return response.send({ filePath, fileName })
+      // return response.send({ filePath, fileName })
     }
   }
 }
