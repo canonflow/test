3fb9ab2490ed4b97d0ba7fc5a4681721651de863
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2023-12-28 23:03:29 +0700

    [From windows 8] testing

--- app/Controllers/Http/UploadsController.ts
+++ app/Controllers/Http/UploadsController.ts
@@ -1,11 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import __dirname from "../../../commands";
+import Application from '@ioc:Adonis/Core/Application'
+import Drive from "@ioc:Adonis/Core/Drive";
 
 export default class UploadsController {
   public async upload({ request, response }: HttpContextContract) {
     const image = request.file('image')
+
     if (image !== null) {
       await image.moveToDisk('./', { name: `test.${image.extname}` })
+      const file = await Drive.get(`./test.${image.extname}`).toString()
+      await Drive.put(Application.appRoot + '/public/assets/files', file)
+      // await image.move(Application.appRoot + '/public/assets/files', {
+      //   name: `test.${image.extname}`,
+      // })
       const fileName = image.fileName
       // const fileTmp = image.tmpPath
       const filePath = image.filePath
